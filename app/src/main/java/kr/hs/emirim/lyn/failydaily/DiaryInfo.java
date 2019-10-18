b0ecdcee537ffc7c378675c86ff07ee0c5dd23de
Sagittta <js1verj@gmail.com>
2019-10-18 09:32:43 +0900

    .

---
+++ app/src/main/java/kr/hs/emirim/lyn/failydaily/DiaryInfo.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.lyn.failydaily;
+
+public class DiaryInfo {
+    String title;
+    String subtitle;
+    String date;
+
+    DiaryInfo(String title, String subtitle, String date) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.date = date;
+    }
+}
--- app/src/main/java/kr/hs/emirim/lyn/failydaily/UseDB.java
+++ app/src/main/java/kr/hs/emirim/lyn/failydaily/UseDB.java
@@ -60,5 +60,4 @@ public class UseDB {
     public void close(){
         mDB.close();
     }
-    // ..
 }
